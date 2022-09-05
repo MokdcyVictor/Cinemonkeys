4ff188ade92fc9d2639d8e811a664331208737c1
MokdcyVictor <familiamokdcy@gmail.com>
2022-09-04 21:19:45 -0400

    commit

--- public/js/Login.js
+++ public/js/Login.js
@@ -1,38 +1,4 @@
 
-    function login() {
-        
-        
-        const email = "admin@gmail.com"
-        const password = "123456"
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            window.location.href = "index.html"
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert("Erro no usuario ou Senha");
-        });
-    }
 
-    function registrar() {
-        const email = "oi@gmail.com"
-        const password = "fdsafdsafdsaf"
-        const auth = getAuth();
-        createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        alert("Usuario cadastrado com Sucesso");
-        // ...
-        })
-        .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-        });
-    }
+
+    
